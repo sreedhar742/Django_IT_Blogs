a0a174fe9f71b94d13b15d0fa55cd5907de48a35
sreedhar742 <psridhar742@gmail.com>
2024-10-03 23:03:04 +0530

    added testcases

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+from .forms import UserRegisterForm
+class Testcasedemo(TestCase):
+    # def setUp(self):
+    #     global username
+    #     global password1
+    #     global  email
+    #     global password2
+    
+    # def test_register(self):
+    #     form=UserRegisterForm(username="farooq",email="psreedhar742@gmail.com",password1="car@123@CAR",password2="car@123@CAR")
+    #     form.save()
+     
+        
+    def test_register(self):
+        url=reverse('register')
+        response=self.client.get(url)
+        self.assertEquals(response.status_code,200)
+        
+    def test_login(self):
+        url=reverse('login')
+        response=self.client.get(url)
+        self.assertEquals(response.status_code,200)
+    
+    
+    
+    
+        
