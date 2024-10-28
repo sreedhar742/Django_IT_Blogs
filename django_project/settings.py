0c1c768e786abf6ceac23e86f7f3073b52896f83
sreedhar742 <psridhar742@gmail.com>
2024-10-28 19:28:32 +0530

    updated Dockerfile

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env()
 SECRET_KEY = 'django-insecure-v%@#)^()f04%m7w!)k!047c0mi)jou#d=_*o_e5h1p3=m+@yv('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
